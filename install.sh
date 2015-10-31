795346a09ca5b043aea8f7b78389a666476c87c9
Guilherme <guilherme@iMac-de-Guilherme.local>
2015-10-31 00:10:49 -0200

    upload demo

--- install.sh
+++ install.sh
@@ -284,8 +284,7 @@ function get_theme_repo {
 	git clone $THEME_SRC wordpress/wp-content/themes/isotopic
 
 	#copia imagens de demonstracao para uploads
-	mkdir wordpress/wp-content/uploads/2015
-	cp -r wordpress/wp-content/themes/isotopic/img/2015 wordpress/wp-content/uploads/2015
+	cp -r wordpress/wp-content/themes/isotopic/img/uploads/* wordpress/wp-content/uploads
 
 	choose_mysql_dump
 }
